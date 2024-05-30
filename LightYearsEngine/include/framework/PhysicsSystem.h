2b562c6085ea77973d68818e15cb60d861379abd
Siranjiv Sivapalan <Siranjiv.s@dmsswe.com>
2024-05-30 16:07:23 +0530

    Create the Add Listener Function

--- LightYearsEngine/include/framework/PhysicsSystem.h
+++ LightYearsEngine/include/framework/PhysicsSystem.h
@@ -4,16 +4,22 @@
 #include "framework/Core.h"
 namespace ly
 {
+	class Actor;
 	//We are using singleton for this
 	class PhysicsSystem
 	{
 	public:
 		static PhysicsSystem& Get();
+		void Step(float deltaTime);
+		b2Body* AddListener(Actor* listener);
+		float GetPhysicsScale() const { return mPhysicsScale; }
 	protected:
 		PhysicsSystem();
 	private:
 		static unique<PhysicsSystem> physicsSystem;
 		b2World mPhysicsWorld;
 		float mPhysicsScale;// We use this because the box2d physics engine uses meters as the size
+		int mVelocityIterations;
+		int mPositionIterations;
 	};
 }
--- LightYearsEngine/src/framework/PhysicsSystem.cpp
+++ LightYearsEngine/src/framework/PhysicsSystem.cpp
@@ -1,4 +1,10 @@
+#include <box2d/b2_body.h>
+#include <box2d/b2_fixture.h>
+#include <box2d/b2_polygon_shape.h>
+
 #include "framework/PhysicsSystem.h"
+#include "framework/Actor.h"
+#include "framework/MathUtility.h"
 
 namespace ly
 {
@@ -14,9 +20,55 @@ namespace ly
 
 		return *physicsSystem;
 	}
+	void PhysicsSystem::Step(float deltaTime)
+	{
+		mPhysicsWorld.Step(deltaTime, mVelocityIterations, mPositionIterations);
+	}
+
+	//The System will figureout a physics body to generate the physics Body
+	b2Body* PhysicsSystem::AddListener(Actor* listener)
+	{
+		//***This is done based on the documentation on how to setup a physics body***
+
+		if (listener->IsPendingDestory()) return nullptr;
+
+		//This is the definition of how the body is
+		b2BodyDef bodyDef;
+		bodyDef.type = b2_dynamicBody;
+
+		//This pointer is the pointer that points to the listener actor
+		//So we get a single when an event happens like overlapping or collition
+		//"reinterpret_cast" is a type of casting operator used to convert one pointer type to another
+		bodyDef.userData.pointer = reinterpret_cast<uintptr_t>(listener);
+		bodyDef.position.Set(listener->GetActorLocation().x * GetPhysicsScale(), listener->GetActorLocation().y * GetPhysicsScale());
+
+		//The Angle should be in radians
+		bodyDef.angle = DegreesToRadians(listener->GetActorRotation());
+
+		b2Body* body = mPhysicsWorld.CreateBody(&bodyDef);
+		
+		b2PolygonShape shape;
+		auto bound = listener->GetActorGlobalBounds();
+		shape.SetAsBox(bound.width/2.f * GetPhysicsScale(), bound.height / 2.f * GetPhysicsScale());
+
+		//Fixture is the actual geometrical body of the physics body
+		//And the Fixture needs to use that definition as it's shape here
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &shape;
+		fixtureDef.density = 1.0f;
+		fixtureDef.friction = 0.3f;
+		fixtureDef.isSensor = true;
+
+		body->CreateFixture(&fixtureDef);
+
+		return body;
+	}
+
 	PhysicsSystem::PhysicsSystem()
 		: mPhysicsWorld{ b2Vec2{0.f,0.f}},
-		mPhysicsScale{0.01f}	// we use this to make it to centemeters
+		mPhysicsScale{0.01f},	// we use this to make it to centemeters
+		mVelocityIterations{8},	//This is based on the documentation of box2d
+		mPositionIterations{3}
 	{
 
 	}
